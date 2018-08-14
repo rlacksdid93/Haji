077703452743f19de47d04ed8df66b39dd4dfa87
R1NRINRIN <42329494+R1NRINRIN@users.noreply.github.com>
2018-08-14 15:27:14 +0900

    Add files via upload

--- haji/app.py
+++ haji/app.py
@@ -1,18 +1,34 @@
 # app.py
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from models import dbMgr
+
 
 
 app = Flask(__name__)
 
+app.secret_key = 'fgyfgyghjhghbkjhkjhkljjklj'
+
 @app.route('/')
 def index():
-    return redirect("/home")
+    return redirect("/homeform")
 
 
-@app.route('/home')
+@app.route('/homeform')
+def homeform():
+    return render_template('homeform.html')
+   
+
+
+@app.route('/home',  methods=["POST"])
 def home():
-    return render_template('home.html')
+    id = request.form['id']
+    pwd = request.form['pwd']
+    data = (id, pwd)
+    torf = dbMgr.login(data)
+    if torf == True : # 로그인 정보 맞음
+        session['id'] = id
+        return render_template('homeform.html', id=id)
+    else : return "<script>alert('비밀번호가 틀려서 되돌아갑니다!');history.back();</script>"
 
 
 @app.route("/joinform")   # id, pwd, name 입력받는 페이지
@@ -27,21 +43,21 @@ def join():
     name = request.form['name']
     data = (id, pwd, name)
     dbMgr.join(data)
-    return redirect('/home')
+    return redirect('/homeform')
 
 
-@app.route('/loginform')
-def loginform():
-    return render_template('loginform.html')
+# @app.route('/loginform')
+# def loginform():
+#     return render_template('loginform.html')
 
 
-@app.route('/login', methods=["POST"])
-def login():
-    id = request.form['id']
-    pwd = request.form['pwd']
-    data = (id, pwd)
-    if dbMgr.login(data) == True : return redirect('/home')
-    else : return "<script>alert('비밀번호가 틀려서 되돌아갑니다!');history.back();</script>"
+# @app.route('/login', methods=["POST"])
+# def login():
+#     id = request.form['id']
+#     pwd = request.form['pwd']
+#     data = (id, pwd)
+#     if dbMgr.login(data) == True : return redirect('/home')
+#     else : return "<script>alert('비밀번호가 틀려서 되돌아갑니다!');history.back();</script>"
 
 
 @app.route('/login_errform')
