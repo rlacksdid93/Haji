ca08b4290c03638c782a863f2737cfd9654e8046
R1NRINRIN <42329494+R1NRINRIN@users.noreply.github.com>
2018-08-14 13:54:04 +0900

    Update app.py

--- haji/app.py
+++ haji/app.py
@@ -1,59 +1,4 @@
-from flask import Flask , render_template, request, redirect
-from models import dbMgr
-
-app = Flask(__name__)
-
-# Web Page 시작부분
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-# 회원가입 Page
-@app.route("/joinform")
-def joinform():
-     return render_template('joinform.html')
-
-#회원가입 정보 저장
-@app.route("/join", methods=['POST'])
-def join():
-    print('넘어온 값들=======================',request.form)
-    id = request.form['id']
-    name = request.form['name']
-    pwd = request.form['pwd']
-    data = (id,pwd,name)
-    dbMgr.join_member(data) 
-    return redirect('/index')
-
-if __name__ =='__main__':
-    app.debug = True 
-    app.run()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    =========================================================================================
-    
-    
-    
-    
-    
-    
-    
-    
-    # app.py
+# app.py
 from flask import Flask, render_template, request, redirect
 from models import dbMgr
 
@@ -70,12 +15,12 @@ def home():
     return render_template('home.html')
 
 
-@app.route("/join")   # id, pwd, name 입력받는 페이지
+@app.route("/joinform")   # id, pwd, name 입력받는 페이지
 def joinform():
     return render_template('joinform.html')
 
 
-@app.route("/join")
+@app.route("/join", methods=["POST"])
 def join():
     id = request.form['id']
     pwd = request.form['pwd']
@@ -87,17 +32,23 @@ def join():
 
 @app.route('/loginform')
 def loginform():
-    return render_template(loginform.html)
+    return render_template('loginform.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST"])
 def login():
     id = request.form['id']
     pwd = request.form['pwd']
-    name = request.form['name']
-    data = (id, pwd, name)
-    dbMgr.login(data)
-    return redirect('/home/logon')
+    data = (id, pwd)
+    if dbMgr.login(data) == True : return redirect('/home')
+    else : return "<script>alert('비밀번호가 틀려서 되돌아갑니다!');history.back();</script>"
+
+
+@app.route('/login_errform')
+def login_errfrom():
+    return render_template(login_errfrom.html)
+
+    
 
 
     
@@ -120,7 +71,3 @@ def login():
 if __name__ == '__main__' :
     app. debug = True
     app.run()
-    
-    
-    
-    
