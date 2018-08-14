efbc497e870c99d51e878eb86256bd05485b230e
R1NRINRIN <42329494+R1NRINRIN@users.noreply.github.com>
2018-08-14 10:03:00 +0900

    Update app.py

--- haji/app.py
+++ haji/app.py
@@ -27,3 +27,100 @@ def join():
 if __name__ =='__main__':
     app.debug = True 
     app.run()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    =========================================================================================
+    
+    
+    
+    
+    
+    
+    
+    
+    # app.py
+from flask import Flask, render_template, request, redirect
+from models import dbMgr
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect("/home")
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route("/join")   # id, pwd, name 입력받는 페이지
+def joinform():
+    return render_template('joinform.html')
+
+
+@app.route("/join")
+def join():
+    id = request.form['id']
+    pwd = request.form['pwd']
+    name = request.form['name']
+    data = (id, pwd, name)
+    dbMgr.join(data)
+    return redirect('/home')
+
+
+@app.route('/loginform')
+def loginform():
+    return render_template(loginform.html)
+
+
+@app.route('/login')
+def login():
+    id = request.form['id']
+    pwd = request.form['pwd']
+    name = request.form['name']
+    data = (id, pwd, name)
+    dbMgr.login(data)
+    return redirect('/home/logon')
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__' :
+    app. debug = True
+    app.run()
+    
+    
+    
+    
